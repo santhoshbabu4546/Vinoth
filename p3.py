2c4c4feed9041d530ab1807e5cb6b826803c0367
vinoth740 <52446455+vinoth740@users.noreply.github.com>
2019-07-03 09:52:52 +0530

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,7 @@
+char=input("")
+if char in ('a','e','i','o','u','A','E','I','O','U'):
+   print("Vowel")
+elif char in ('B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',' S', 'T', 'V', 'X', 'Z','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',' s', 't', 'v', 'x', 'z') :
+   print("Consonant")
+else:
+  print("invalid")
