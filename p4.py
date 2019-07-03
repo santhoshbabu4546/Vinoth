72d27760725566b8f1856a7be39d85d7656eab1f
vinoth740 <52446455+vinoth740@users.noreply.github.com>
2019-07-03 10:01:05 +0530

    Create p4.py

---
+++ p4.py
@@ -0,0 +1,5 @@
+char=input("")
+if char in ('a','e','i','o','u','A','E','I','O','U','B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',' S', 'T', 'V', 'X', 'Z','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',' s', 't', 'v', 'x', 'z'):
+   print("Alphabet")
+else:
+  print("No")
